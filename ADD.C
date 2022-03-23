ff9b0a4f836b4220f2eeda1a4b58b5e7e11b14eb
kirtigirase19 <kirtirgirase@gmail.com>
2022-03-23 08:50:12 +0530

    ADDED:[kirti]program for Addition of two Number

---
+++ ADD.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\nEnter the first number=");
+	scanf("%d",&a);
+	printf("\nEnter the second number=");
+	scanf("%d",&b);
+	c=a+b;
+	printf("\n Addition =%d",c);
+	getch();
+}
