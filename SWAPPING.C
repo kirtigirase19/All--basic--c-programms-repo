02803b1ddc28b989a182c67df2a4d80fac4e531e
kirtigirase19 <kirtirgirase@gmail.com>
2022-03-24 08:54:32 +0530

    ADDED:[Kirti] Swapping of two numbers

---
+++ SWAPPING.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=20,b=10,c;
+	clrscr();
+	c=a;
+	a=b;
+	b=c;
+	printf("\nThe value of a= ");
+	printf("\n The value of b= ");
+	getch();
+}
+
+
